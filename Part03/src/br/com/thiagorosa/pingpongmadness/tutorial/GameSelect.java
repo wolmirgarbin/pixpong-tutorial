ab7c1245868844d10c4254a508c2fc52edb51075
thiago.rosa <thiago.rosa@gmail.com>
2011-11-21 23:18:04 +0000

    * Part03 - sequence class, sequence_001 xml, sequence_002 xml, sequence_003 xml * Part02 - version incremented * Releases - part 03 added

---
+++ Part03/src/br/com/thiagorosa/pingpongmadness/tutorial/GameSelect.java
@@ -0,0 +1,14 @@
+package br.com.thiagorosa.pingpongmadness.tutorial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameSelect extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.level_list);
+    }
+
+}
